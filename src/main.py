70b28b02f576eb6052fb3d0d4f68347d8ebae1ec
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-03-01 10:50:53 -0600

    parse data into dictionary

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import requests
+import json
 
 def main():
     print("HankStats")
     r = requests.get('http://10.0.0.50/RAWSTATUS')
     print(r.status_code)
-    print(r.content)
+    #print(r.content)
+
+    snake_data = json.loads(r.content)
+    print(snake_data)
 
 if __name__ == "__main__":
     main()
